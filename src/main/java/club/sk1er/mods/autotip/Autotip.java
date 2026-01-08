4f8baa03d7848fea04f368a7420566bd2912cde6
QWERTZexe <qwertz@qwertz.app>
2026-01-08 18:18:40 +0100

    Initial commit
    Basic Fabric 1.21.8 mod structure

---
+++ src/main/java/club/sk1er/mods/autotip/Autotip.java
@@ -0,0 +1,10 @@
+package club.sk1er.mods.autotip;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Autotip implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
